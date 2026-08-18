a8f5d1b188f258ecb4d93eb0abcb3cc0870733e9
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2026-08-18 17:52:48 -0300

    chore: Added post its colors

--- app/src/main/java/br/com/rodroid/notas/presentation/ui/theme/Theme.kt
+++ app/src/main/java/br/com/rodroid/notas/presentation/ui/theme/Theme.kt
@@ -11,15 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Colors.Purple80,
+    secondary = Colors.PurpleGrey80,
+    tertiary = Colors.Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Colors.Purple40,
+    secondary = Colors.PurpleGrey40,
+    tertiary = Colors.Pink40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
