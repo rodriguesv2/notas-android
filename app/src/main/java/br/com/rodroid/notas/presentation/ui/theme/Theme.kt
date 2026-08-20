535df2a758482250a42daa64418ce09c55f96988
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2026-08-20 00:26:40 -0300

    feat: Adjusted theme to not reflect wallpaper

--- app/src/main/java/br/com/rodroid/notas/presentation/ui/theme/Theme.kt
+++ app/src/main/java/br/com/rodroid/notas/presentation/ui/theme/Theme.kt
@@ -25,22 +25,12 @@ private val LightColorScheme = lightColorScheme(
     tertiary = Colors.Pink40,
     background = Color(0xffF7F4EB),
     surface = Color(0xffF7F4EB),
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun NotasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
